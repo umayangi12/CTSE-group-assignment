14abe4e2250040502f8ef8fa2b373c31cb88e9b4
Umayangi Ekanayake <89120566+umayangi12@users.noreply.github.com>
2023-03-23 13:23:39 +0530

    done some styling

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -52,21 +52,44 @@ const Dashboard = () => {
           onPress={() => {
             firebase.auth().signOut();
           }}
-          style={styles.button}
+          style={styles.buttonSignout}
         >
-          <Text style={{ fontSize: 22, fontWeight: "700" }}>Sign Out</Text>
+          <Text style={{ fontSize: 12, fontWeight: "700" }}>Sign Out</Text>
         </TouchableOpacity>
+
+        <Text style={{fontSize: 13, fontWeight: "700", marginLeft: -270, marginTop: 20, marginBottom: -25}}>Quick Access</Text>
+
         <TouchableOpacity
           onPress={() => navigation.navigate("Home")}
-          style={styles.button}
+          style={styles.buttonDoctor}
         >
-          <Text style={{ fontSize: 22, fontWeight: "700" }}>Doctor</Text>
+          <Text style={{ fontSize: 14, fontWeight: "700", color: "#fff" }}>
+            Doctor Prescription
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => navigation.navigate("ViewAppointment")}
-          style={styles.button}
+          style={styles.buttonPatient}
         >
-          <Text style={{ fontSize: 22, fontWeight: "700" }}>Patient</Text>
+          <Text style={{ fontSize: 14, fontWeight: "700", color: "#fff" }}>
+            Doctor Channeling
+          </Text>
+        </TouchableOpacity>   
+        <TouchableOpacity
+          onPress={() => navigation.navigate("")}
+          style={styles.buttonPharmacy}
+        >
+          <Text style={{ fontSize: 14, fontWeight: "700", color: "#fff" }}>
+            Pharmacy
+          </Text>
+        </TouchableOpacity>        
+        <TouchableOpacity
+          onPress={() => navigation.navigate("")}
+          style={styles.buttonLab}
+        >
+          <Text style={{ fontSize: 14, fontWeight: "700", color: "#fff" }}>
+            Laboratory
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -74,9 +97,9 @@ const Dashboard = () => {
           style={styles.button}
         >
           <Text style={{ fontSize: 22, fontWeight: "700" }}>
-            BMI Calculator
+            BMI Calculator 
           </Text>
-        </TouchableOpacity>
+        </TouchableOpacity>        
       </SafeAreaView>
     </ScrollView>
   );
@@ -99,7 +122,57 @@ const styles = StyleSheet.create({
     backgroundColor: "#026efd",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 50,
+    borderRadius: 20,
+  },
+  buttonDoctor: {
+    marginTop: 30,
+    height: 50,
+    width: 180,
+    backgroundColor: "#647DE0",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginLeft: -180,
+  },
+  buttonPatient: {
+    marginTop: -50,
+    height: 50,
+    width: 180,
+    backgroundColor: "#2A3A7D",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginLeft: 180,
+  },
+  buttonPharmacy: {
+    marginTop: 6,
+    height: 50,
+    width: 180,
+    backgroundColor: "#227131",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginLeft: -180,
+  },
+  buttonLab: {
+    marginTop: -50,
+    height: 50,
+    width: 180,
+    backgroundColor: "#6AD27E",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginLeft: 180,
+  },
+  buttonSignout: {
+    marginTop: 50,
+    height: 40,
+    width: 90,
+    backgroundColor: "#026efd",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    marginLeft: 250,
   },
   scrollView: {
     backgroundColor: "#fff",
@@ -114,6 +187,12 @@ const styles = StyleSheet.create({
     width: 400,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
-    position: 'absolute',
+    position: "absolute",
+  },
+  quickText: {
+    marginTop: 10,
+    marginLeft: 20,
+
+
   },
 });
